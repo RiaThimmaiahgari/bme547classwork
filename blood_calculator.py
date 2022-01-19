8f25c35c2f87fa901f0288fed702e64a2c321bce
Ria Thimmaiahgari <ria.thimmaiahgari@duke.edu>
2022-01-19 08:02:42 -0500

    Added function to accept user input (generic test)

--- blood_calculator.py
+++ blood_calculator.py
@@ -9,4 +9,8 @@ def interface():
             keep_running = False
     return 
    
+def accept_input(test_result):
+    entry = input("Enter the {} test result".format(test_result))
+    return int(entry)
+
 interface()
