5da9d074240054007a68b9a4c4eb2b7ec4d21f60
Ria Thimmaiahgari <ria.thimmaiahgari@duke.edu>
2022-01-19 07:52:59 -0500

    Modified blood_calculator.py with while loop so that program only quits when user inputs 9

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,9 +1,12 @@
 def interface():
-    print("My Program")
-    print("Options:")
-    print("9 - Quit")
-    choice = input("Enter your choice: ")
-    if choice=='9':
-        return
+    print("Blood Test Analysis")
+    keep_running = True
+    while keep_running:
+        print("Options:")
+        print("9 - Quit")
+        choice = input("Enter your choice: ")
+        if choice=='9':
+            keep_running = False
+    return 
    
 interface()
